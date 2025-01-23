4e2d5d37b3e7ccd0e0734081efcf765ae3dec256
JMSH <41555054+jmsett@users.noreply.github.com>
2025-01-23 23:13:03 +0100

    Update splitstep.jl
    Added FFT-based method

--- src/splitstep.jl
+++ src/splitstep.jl
@@ -7,6 +7,9 @@
 
 # add alg: for real-valued functions (faster compiling) "alg="realvalued""
 
+###################################
+### QTT-based Split-Step method ###
+
 function qtt_splitsteps(v::Vector{ComplexF64}, n::Int, xrange::Tuple{Float64, Float64}, Δt::Float64, tsteps::Int, frange::String="complex"; kwargs...)
 	# Simulation parameters
 	N = 2^n # length of vector (assert its a power of 2)
@@ -32,6 +35,34 @@ function qtt_splitsteps(v::Vector{ComplexF64}, n::Int, xrange::Tuple{Float64, Fl
     		ℱψₜ = apply_dft_mpo(ψₜ; kwargs...)
     		k²ℱψₜ = apply(Dₓₓ_mpo,ℱψₜ; kwargs...)
     		ψₜ = apply_idft_mpo(k²ℱψₜ; kwargs...)
+	end
+	return ψₜ
+
+###################################
+### FFT-based Split-Step method ###
+	
+function fft_splitsteps(v::Vector{ComplexF64}, n::Int, xrange::Tuple{Float64, Float64}, Δt::Float64, tsteps::Int, frange::String="complex")
+	# Simulation parameters
+	N = 2^n # length of vector (assert its a power of 2)
+	x = range(xrange[1], xrange[2], length=N) # use ITensorQTT::xqttrange?
+	Δx = x[2]-x[1]
+	
+	# Squared frequencies/ Fourier coefficients
+	k² = ((2*pi*fftfreq(N))./Δx).^2
+	Dₓₓ = [exp(1im*Δt*i) for i in k²]
+	
+	# Nonlinear scalar term
+	λ(ψ_var) = exp(1im*Δt*(2*dot(ψ_var,ψ_var)))^(1/n)
+	
+	# Initial state
+	ψₜ .= v
+	
+	# Split-Step loop
+	for t in 1:tsteps
+   		# Nonlinear term (scalar mult.) and derivative (Diagonal MPO contraction) in Fourier space
+    		ψₜ = ψₜ .* λ(ψₜ)
+    		k²ℱψₜ = fft(ψₜ) .* Dₓₓ
+    		ψₜ = ifft(k²ℱψₜ)
 	end
 	return ψₜ
 end
